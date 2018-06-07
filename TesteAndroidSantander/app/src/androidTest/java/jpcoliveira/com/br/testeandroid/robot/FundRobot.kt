3f402e2d8ffc991dcfb08c892a2545c10232afa6
Joao Oliveira <joaopaulo_391@hotmail.com>
2018-06-07 09:28:10 -0300

    continuidade na implementacao dos testes do fragmento de fundos de investimento

--- TesteAndroidSantander/app/src/androidTest/java/jpcoliveira/com/br/testeandroid/robot/FundRobot.kt
+++ TesteAndroidSantander/app/src/androidTest/java/jpcoliveira/com/br/testeandroid/robot/FundRobot.kt
@@ -1,42 +1,55 @@
 package jpcoliveira.com.br.testeandroid.robot
 
+import android.content.Intent
+import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.scrollTo
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
-import android.system.Os.bind
-import com.github.salomonbrys.kodein.*
-import jpcoliveira.com.br.testeandroid.FileReader
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.bind
+import com.github.salomonbrys.kodein.singleton
+import jpcoliveira.com.br.testeandroid.util.FileReader
 import jpcoliveira.com.br.testeandroid.R
+import jpcoliveira.com.br.testeandroid.asApp
 import jpcoliveira.com.br.testeandroid.dashboard.MainActivity
-import okhttp3.internal.Internal.instance
+import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
 import org.hamcrest.Matchers.allOf
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class FundRobot(val server: MockWebServer, val rule: ActivityTestRule<MainActivity>?) {
 
-    val dispatcher = object : Dispatcher() {
-        override fun dispatch(request: RecordedRequest?): MockResponse {
-            if (request?.path?.contains("cells.json")!!) {
-                val json = FileReader.readFile("local_cells.json")
-                return MockResponse().setResponseCode(200).setBody(json)
-            } else if (request.path.contains("fund.json")) {
-                val json = FileReader.readFile("local_fund.json")
-                return MockResponse().setResponseCode(200).setBody(json)
-            } else {
-                return MockResponse().setResponseCode(404)
-            }
-        }
-    }
+class FundRobot(val rule: ActivityTestRule<MainActivity>?) {
+
+    var server: MockWebServer? = null
+    val app = InstrumentationRegistry.getInstrumentation().targetContext.asApp()
 
     fun prepare(): FundRobot {
-        Thread.sleep(3000L)
 
+        server = MockWebServer()
+        server?.setDispatcher(dispatcher)
+        server?.start()
+        val url = server?.url("/").toString()
+
+        app.injection()
+        app.addModule(
+                Kodein.Module(allowSilentOverride = true) {
+                    bind<Retrofit.Builder>() with singleton {
+                        Retrofit.Builder()
+                                .baseUrl(url)
+                                .client(OkHttpClient())
+                                .addConverterFactory(GsonConverterFactory.create())
+                    }
+                }
+        )
+
+        rule?.launchActivity(Intent())
         return this
     }
 
@@ -86,27 +99,44 @@ class FundRobot(val server: MockWebServer, val rule: ActivityTestRule<MainActivi
     }
 
     fun checkRecyclerViewFundIsDisplayed(): FundRobot {
+        onView(withId(R.id.recyclerViewFund)).perform(scrollTo())
         onView(withId(R.id.recyclerViewFund)).check(matches(isDisplayed()))
         return this
     }
 
     fun checkInfoTitle(): FundRobot {
-        onView(withId(R.id.info_title)).check(matches(withText("Mais informações sobre o investimento")))
+        onView(withId(R.id.info_title))
+                .check(matches(withText("Mais informações sobre o investimento")))
         return this
     }
 
     fun checkMoreInfoFundMonth(): FundRobot {
-        onView(withId(R.id.more_info_fund_month)).check(matches(withText("0.3%")))
+        onView(withId(R.id.more_info_fund_month))
+                .check(matches(withText("0.3%")))
         return this
     }
 
     fun checkMoreInfoCdiMonth(): FundRobot {
-        onView(withId(R.id.more_info_cdi_month)).check(matches(withText("0.3%")))
+        onView(withId(R.id.more_info_cdi_month))
+                .check(matches(withText("0.3%")))
         return this
     }
 
-    fun checkInvestimentTabLayoutIsDisplayed(): FundRobot {
-        onView(allOf(withId(R.id.tabLayout), withText("Investimento"))).check(matches(isDisplayed()))
-        return this
+    fun tearDown() {
+        server?.shutdown()
+    }
+
+    val dispatcher = object : Dispatcher() {
+        override fun dispatch(request: RecordedRequest?): MockResponse {
+            if (request?.path?.contains("cells.json")!!) {
+                val json = FileReader.readFile(app, "local_cells_1.json")
+                return MockResponse().setResponseCode(200).setBody(json)
+            } else if (request.path.contains("fund.json")) {
+                val json = FileReader.readFile(app, "local_fund_1.json")
+                return MockResponse().setResponseCode(200).setBody(json)
+            } else {
+                return MockResponse().setResponseCode(404)
+            }
+        }
     }
 }
--- TesteAndroidSantander/app/src/androidTest/java/jpcoliveira/com/br/testeandroid/ui/FundTest.kt
+++ TesteAndroidSantander/app/src/androidTest/java/jpcoliveira/com/br/testeandroid/ui/FundTest.kt
@@ -1,60 +1,39 @@
 package jpcoliveira.com.br.testeandroid.ui
 
-import android.app.Instrumentation
-import android.content.Intent
+import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.bind
-import com.github.salomonbrys.kodein.singleton
-import jpcoliveira.com.br.testeandroid.FileReader
 import jpcoliveira.com.br.testeandroid.dashboard.MainActivity
-import jpcoliveira.com.br.testeandroid.di.BASE_URL
-import jpcoliveira.com.br.testeandroid.di.apiModule
 import jpcoliveira.com.br.testeandroid.robot.FundRobot
-import okhttp3.OkHttpClient
-import okhttp3.mockwebserver.Dispatcher
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
-import okhttp3.mockwebserver.RecordedRequest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-
+@LargeTest
 @RunWith(AndroidJUnit4::class)
-class FundTest : Instrumentation() {
+class FundTest {
 
-    //    @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
+    @Rule
+    @JvmField
+    val activityRule =
+            ActivityTestRule(
+                    MainActivity::class.java,
+                    true,
+                    false
+            )
+
     var robot: FundRobot? = null
-
-    var server: MockWebServer? = null
 
     @Before
     fun setUp() {
-        server = MockWebServer()
-        server?.setDispatcher(dispatcher)
-        server?.start()
-
-        BASE_URL = server?.url("/").toString()
-
-        robot = FundRobot(server, null)
+        robot = FundRobot(activityRule)
     }
 
     @Test
     fun shouldCheckShare_isVisible() {
-
-        val intent = Intent(activityRule.activity, MainActivity::class.java)
-
-        activityRule.launchActivity(intent)
-
-        robot?.prepare()
-                ?.checkButtonShareIsVisible()
+        robot?.prepare()?.checkButtonShareIsVisible()
     }
 
     @Test
@@ -74,8 +53,6 @@ class FundTest : Instrumentation() {
 
     @Test
     fun shouldCheckTextWhatIs_isOK() {
-
-
         robot?.prepare()?.checkTextWhatIs()
     }
 
@@ -96,7 +73,9 @@ class FundTest : Instrumentation() {
 
     @Test
     fun shouldCheckRecyclerViewFund_isDisplayed() {
-        robot?.prepare()?.checkRecyclerViewFundIsDisplayed()
+        robot
+                ?.prepare()
+                ?.checkRecyclerViewFundIsDisplayed()
     }
 
     @Test
@@ -114,27 +93,9 @@ class FundTest : Instrumentation() {
         robot?.prepare()?.checkMoreInfoCdiMonth()
     }
 
-    @Test
-    fun shouldCheckItemtabLayoutInvestiment_isDisplayed() {
-        robot?.prepare()?.checkInvestimentTabLayoutIsDisplayed()
-    }
-
     @After
     fun tearDown() {
+        robot?.tearDown()
         robot = null
-    }
-
-    val dispatcher = object : Dispatcher() {
-        override fun dispatch(request: RecordedRequest?): MockResponse {
-            if (request?.path?.contains("cells.json")!!) {
-                val json = FileReader.readFile("local_cells.json")
-                return MockResponse().setResponseCode(200).setBody(json)
-            } else if (request.path.contains("fund.json")) {
-                val json = FileReader.readFile("local_fund.json")
-                return MockResponse().setResponseCode(200).setBody(json)
-            } else {
-                return MockResponse().setResponseCode(404)
-            }
-        }
     }
 }
