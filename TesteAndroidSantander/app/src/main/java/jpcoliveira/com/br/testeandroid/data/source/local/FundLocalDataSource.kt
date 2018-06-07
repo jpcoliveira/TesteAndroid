ccbd15a2c4cb93634feb77497e21cce251512796
Joao Oliveira <joaopaulo_391@hotmail.com>
2018-06-07 09:28:53 -0300

    refatoracao

--- TesteAndroidSantander/app/src/main/java/jpcoliveira/com/br/testeandroid/data/source/local/FundLocalDataSource.kt
+++ TesteAndroidSantander/app/src/main/java/jpcoliveira/com/br/testeandroid/data/source/local/FundLocalDataSource.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import com.google.gson.Gson
 import jpcoliveira.com.br.testeandroid.data.source.FundContractDataSource
 import jpcoliveira.com.br.testeandroid.fund.model.Fund
+import jpcoliveira.com.br.testeandroid.util.FileReader
 import java.io.IOException
-import java.nio.charset.Charset
 
 
 class FundLocalDataSource(val context: Context?) : FundContractDataSource<Fund> {
@@ -16,27 +16,11 @@ class FundLocalDataSource(val context: Context?) : FundContractDataSource<Fund> 
     private fun parseJsonToFund(success: (fund: Fund?) -> Unit, failure: (throwable: Throwable?) -> Unit) {
         val gson = Gson()
         try {
-            val json = getJsonByFileName("local_fund.json")
+            val json = FileReader.readFile(context!!,"local_fund.json")
             val fund = gson.fromJson(json, Fund::class.java)
             success(fund)
         } catch (e: IOException) {
             failure(e)
         }
-    }
-
-    private fun getJsonByFileName(fileName: String): String? {
-        var json: String? = null
-
-        try {
-            val inputStream = context?.assets?.open(fileName)
-            val size = inputStream?.available()
-            val buffer = ByteArray(size ?: 0)
-            inputStream?.read(buffer)
-            inputStream?.close()
-            json = String(buffer, Charset.forName("UTF-8"))
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return json
     }
 }
